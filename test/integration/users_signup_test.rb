87af43da52ec947f26cedd7e816949e843ad6667
RichStone <richard.had@hotmail.de>
2020-06-01 11:33:20 +0200

    add error handling

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "invalid@email",
+          password: "short",
+          password_confirmation: "wrong"
+        }
+      }
+    end
+    assert_template 'users/new'
+  end
+
+end
