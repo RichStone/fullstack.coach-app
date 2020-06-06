b19b956a45a302831dd05a8430b9afb2d80a81c6
RichStone <richard.had@hotmail.de>
2020-06-06 15:54:20 +0200

    add user login persistency across sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 
-  def self.digest(string)
+  def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? 
       BCrypt::Engine::MIN_COST :
       BCrypt::Engine.cost
@@ -17,12 +17,24 @@ class User < ApplicationRecord
   end
 
   # Returns a random token
-  def self.new_token
+  def User.new_token
     SecureRandom.urlsafe_base64
   end
 
+  # remembers a user in the database for use in persistent sessions
   def remember
-    self.remember_token = new_token
-    update_attribute(:remember_digest, digest(remember_token))
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_token).is_password?(remember_token)
+  end
+
+  # forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
