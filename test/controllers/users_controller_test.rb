30d9d883d947403eb20ac978a74eec3d1d44cce4
RichStone <richard.had@hotmail.de>
2020-06-16 07:35:52 +0200

    add Students list page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,10 +5,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
     @other_user = users(:archer)
   end
-  
+
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+
+  test"should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
   end
 
   test "should redirect edit when not logged in" do
@@ -16,7 +21,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-  
+
   test "should redirect update when not logged in" do
     patch user_path(@user), params: {
       user: {
@@ -27,14 +32,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-  
+
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
     assert flash.empty?
     assert_redirected_to root_url
   end
-  
+
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
     patch user_path(@user), params: {
@@ -46,7 +51,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
-  
+
   test "should friendly redirect" do
     get edit_user_path(@user)
     log_in_as(@user)
